9bace204d1eb252f3c4cf6d2034d8d9aab81efda
Siraj <ss1425306@gmail.com>
2024-12-27 21:53:44 +0530

    Implement add,update,del,complete. functionality with context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-
+import { useState } from 'react'
 import './App.css'
+import { TodoProvider } from './context'
 
 function App() {
+  const [todos,setTodos] = useState([])
+
+  const addTodo =(todo)=>{
+    setTodos((prev)=>[...prev,{id:prev.length+1,todo:'New Todo',completed:false}])
+  }
+
+  const updateTodo =(id,todo)=>{
+    // setTodos((prev)=> (prev.map((item)=>item.id===id?{...item,todo}:item)))
+    setTodos((prev)=>prev.map((prevTodo)=>prevTodo.id=== id ? todo : prevTodo))
+  }
+  const deleteTodo =(id)=>{
+    setTodos((prev)=>prev.filter((todo)=>todo.id!==id))
+  }
+  const toggleComplete =(id)=>{
+    setTodos((prev)=>prev.map((todo)=> 
+      todo.id===id ? {...todo, completed: !todo.completed} : todo))
+  }
+
+
   return (
-    <>
-       <h1 className='bg-blue-900'>aslk;fj</h1>
-    </>
+    <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
+       <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+            <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+            <div className="mb-4">
+                {/* Todo form goes here */} 
+            </div>
+            <div className="flex flex-wrap gap-y-3">
+                {/*Loop and Add TodoItem here */}
+            </div>
+        </div>
+    </div>
+    </TodoProvider>
   )
 }
 
