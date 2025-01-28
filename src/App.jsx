58fdd40c842cb070408911c82d7603fa67255feb
Siraj <ss1425306@gmail.com>
2025-01-28 13:23:49 +0530

    Add TodoForm and TodoItem components; implement localStorage sync for todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
 import { TodoProvider } from './context'
 
@@ -6,7 +6,7 @@ function App() {
   const [todos,setTodos] = useState([])
 
   const addTodo =(todo)=>{
-    setTodos((prev)=>[...prev,{id:prev.length+1,todo:'New Todo',completed:false}])
+    setTodos((prev)=>[...prev,{id:prev.length+1,...todo}])
   }
 
   const updateTodo =(id,todo)=>{
@@ -21,6 +21,16 @@ function App() {
       todo.id===id ? {...todo, completed: !todo.completed} : todo))
   }
 
+  useEffect(() => {
+   const todos= JSON.parse(localStorage.getItem('todos'))
+   if (todos && todos.length > 0) {
+    setTodos(todos)
+   }
+  }, [])
+  
+  useEffect(()=>{
+    localStorage.setItem("todos",JSON.stringify(todos))
+  },[todos])
 
   return (
     <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
